79bbcb3d089fa87902cb74e168dbf4a6951f73e8
Dylan Norris <dylannorris919@gmail.com>
2025-04-10 11:12:43 -0500

    takes in url and json file and prints it

--- requestSender.py
+++ requestSender.py
@@ -1,1 +1,14 @@
+import json
 
+url = input("What is the url? ")
+
+fileName = input("Name of file with JSON object: ")
+
+print(url + " " + fileName)
+
+file = open(fileName)
+
+with open(fileName) as file:
+    jsonPayload = json.load(file)
+
+print(jsonPayload)
